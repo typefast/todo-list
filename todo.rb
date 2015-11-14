e5bc5f2c682d7adcf90f39193ad9b7241f81804f
Harry Oliver <someonecancode@yahoo.co.uk>
2015-11-14 20:17:25 +0000

    Add ToDoList class and initialize

--- todo.rb
+++ todo.rb
@@ -1,1 +1,16 @@
 #todolist
+#create a hash to store the list
+#add an item to the list
+#display the list
+#delete an item from the list
+#save the list
+#load the list - on startup? check previous projects
+#mark an item as done
+
+class ToDoList
+  def initialize
+    @list = {}
+  end
+end
+
+list = ToDoList.new

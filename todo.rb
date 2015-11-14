08dcc5188673a36b349f254ddf67da007f6f5dce
Harry Oliver <someonecancode@yahoo.co.uk>
2015-11-14 20:24:43 +0000

    Add_item method

--- todo.rb
+++ todo.rb
@@ -8,9 +8,17 @@
 #mark an item as done
 
 class ToDoList
+  attr_reader :list
   def initialize
     @list = {}
+  end
+  
+  def add_item(list_item, description)
+    list[list_item] = description
   end
 end
 
 list = ToDoList.new
+list.add_item("buy petrol", "Drive to petrol station")
+p list.list
+
